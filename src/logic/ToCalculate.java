4c183e1ad35371469c1205cf0f2278a83ad6c7d2
Mllege <kiry.kkv12@gmail.com>
2019-11-20 00:58:09 +0700

    calculate

--- src/logic/ToCalculate.java
+++ src/logic/ToCalculate.java
@@ -2,10 +2,4 @@ package logic;
 
 public class ToCalculate {
     //переменные из пользовательского ввода
-    private int x;
-    public int getX(){
-        this.x = x;
-        return x;
     }
-
-}
