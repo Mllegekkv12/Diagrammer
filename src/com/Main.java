7a776e0e92579e9fa11e1e2c95f3c2dfde0f9d08
Maxim Maslo <maxxmaslo@gmail.com>
2019-11-20 01:20:01 +0700

    class Main change

--- src/com/Main.java
+++ src/com/Main.java
@@ -11,8 +11,8 @@ public class Main extends Diagram_Window {
     }
 
     public static void main(String[] args) {
-        Diagram_Window DW = new Diagram_Window(800, 1000);
-        new Menu_Window();
+        Diagram_Window DW = new Diagram_Window(400, 500);
+        new Menu_Window(300, 400);
         Scanner inputX = new Scanner(System.in);
         Scanner inputY = new Scanner(System.in);
         DW.count(inputX.nextDouble(), inputY.nextDouble(), 0, 0); //Для не статик функции нужно создание объекта класса
