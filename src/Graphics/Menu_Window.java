a5d8b7d0f2edbedbc408bc451b092e02bfcced85
Maxim Maslo <maxxmaslo@gmail.com>
2019-11-20 06:01:28 +0700

    Action events 2

--- src/Graphics/Menu_Window.java
+++ src/Graphics/Menu_Window.java
@@ -7,8 +7,6 @@ import logic.ToCalculate;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 
 public class Menu_Window extends WindowFrame implements ToCalculate {
     public Menu_Window(int width, int height) {
@@ -18,18 +16,18 @@ public class Menu_Window extends WindowFrame implements ToCalculate {
         AbstractAction action = new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                double n = Double.parseDouble(textField.getText());
+                System.out.println(n);
                 textField.setText(""); //TODO ДОБАВИТЬ ЦИКЛ ВВОДА ЗНАЧЕНИЙ В МАССИВ ПО НАЖАТИЮ ENTER
                 repaint();
                 revalidate();
-                System.out.println("some action");
-                // double inputX = textField;
-                count(5, 3, 0, 0); //TODO ПЕРЕПИСАТЬ ЛОГИКУ ДЛЯ ДИНАМИЧЕСКОГО МАССИВА (С ЦИКЛОМ)
+                count(n, 2, 0, 0); //TODO ПЕРЕПИСАТЬ ЛОГИКУ ДЛЯ ДИНАМИЧЕСКОГО МАССИВА (С ЦИКЛОМ)
             }
         };
 
-        textField.addActionListener( action );
+        textField.addActionListener(action);
         JButton button = new JButton("Calculate");
-        button.addActionListener( action );
+        button.addActionListener(action);
         f.setSize(width, height);
         f.setLayout(new FlowLayout(FlowLayout.LEFT));
         f.add(new Label("Введите данные:"));
