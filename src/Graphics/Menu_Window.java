3b652dced9e3f7c2c0faee89efa2aa4288ebd9df
Maxim Maslo <maxxmaslo@gmail.com>
2019-11-20 06:11:14 +0700

    Action events 3

--- src/Graphics/Menu_Window.java
+++ src/Graphics/Menu_Window.java
@@ -7,29 +7,33 @@ import logic.ToCalculate;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 
 public class Menu_Window extends WindowFrame implements ToCalculate {
     public Menu_Window(int width, int height) {
         super(width, height);
         final Frame f = new Frame("Diagrammer");
         JTextField textField = new JTextField(20);
+        JButton button = new JButton("Calculate");
+        AbstractAction changewindow = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Тут будет открываться окно с готовой диаграммой; возможно здесь будет функция подсчета");
+            }
+        };
         AbstractAction action = new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                double n = Double.parseDouble(textField.getText());
+                System.out.println(n);
                 textField.setText(""); //TODO ДОБАВИТЬ ЦИКЛ ВВОДА ЗНАЧЕНИЙ В МАССИВ ПО НАЖАТИЮ ENTER
                 repaint();
                 revalidate();
-                System.out.println("some action");
-                // double inputX = textField;
-                count(5, 3, 0, 0); //TODO ПЕРЕПИСАТЬ ЛОГИКУ ДЛЯ ДИНАМИЧЕСКОГО МАССИВА (С ЦИКЛОМ)
+                count(n, 2, 0, 0); //TODO ПЕРЕПИСАТЬ ЛОГИКУ ДЛЯ ДИНАМИЧЕСКОГО МАССИВА (С ЦИКЛОМ)
             }
         };
 
-        textField.addActionListener( action );
-        JButton button = new JButton("Calculate");
-        button.addActionListener( action );
+        textField.addActionListener(action);
+        button.addActionListener(changewindow);
         f.setSize(width, height);
         f.setLayout(new FlowLayout(FlowLayout.LEFT));
         f.add(new Label("Введите данные:"));
