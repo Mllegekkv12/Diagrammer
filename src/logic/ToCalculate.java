12bc3310ea53cea7eab8e024f1a4ec5abc1642b5
Maxim Maslo <maxxmaslo@gmail.com>
2019-11-19 20:30:18 +0700

    Success2

--- src/logic/ToCalculate.java
+++ src/logic/ToCalculate.java
@@ -2,5 +2,10 @@ package logic;
 
 public class ToCalculate {
     //переменные из пользовательского ввода
+    private int x;
+    public int getX(){
+        this.x = x;
+        return x;
+    }
 
 }
