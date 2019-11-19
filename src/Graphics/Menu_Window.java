536b1733a3fde79149035906dec41f59e4c9c937
Mllege <kiry.kkv12@gmail.com>
2019-11-20 00:49:57 +0700

    Add button to Menu_Windows2.0

--- src/Graphics/Menu_Window.java
+++ src/Graphics/Menu_Window.java
@@ -2,23 +2,32 @@ package Graphics;
 
 import abstraction.WindowFrame;
 import javax.swing.*;
+import java.awt.*;
 public class Menu_Window extends WindowFrame {
     public Menu_Window(int width, int height) {
         super(width, height);
-        JButton button1 = new JButton("Button 1");
-
+//        JLabel
+        JButton button1 = new JButton("Calculate");
+        button1.setBorderPainted(true);
+        button1.setFocusPainted(false);
+        button1.setContentAreaFilled(false);
         JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        panel.setLayout(new GridLayout(1,1,1,10));
+        Box box = Box.createVerticalBox();
+        box.add(Box.createVerticalStrut(250));
 
-        button1.setAlignmentX(JComponent.RIGHT_ALIGNMENT);
-
+        setContentPane(box);
         panel.add(button1);
-
         add(panel);
+
+
+
+
 
 
     }
 
+    public class Action{}
 
 
 }
