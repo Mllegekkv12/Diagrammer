d6e9ca27f8426494ff3ea42d0dbb4810a65f1a14
Maxim Maslo <maxxmaslo@gmail.com>
2019-11-20 19:53:46 +0700

    Diagram logic 3

--- src/Graphics/Diagram_Window.java
+++ src/Graphics/Diagram_Window.java
@@ -19,37 +19,36 @@ public class Diagram_Window extends WindowFrame implements ToCalculate {
     Diagram_Window(int width, int height) {
         super(width, height);
     }
-    private ArrayList<Double> sectors = new ArrayList<>();
-    private ArrayList<Double> proc = new ArrayList<>();
-    @Override
-    public double count(ArrayList<Double> list, double sum, double sector) {
-        double tm ;
-        int j = 0 ;
-        for (Double eDouble: list){
-              sum+=eDouble;
-          }
-        for (Double sDouble : list){
-            tm = sDouble/sum;
-            sectors.add(tm);
-            j++;
-        }
-//        System.out.println(sectors.size() + " size sector");
-//        System.out.println(j + " j");
-//        System.out.println(sectors + " sector");
-//        System.out.println(sectors.size() - j + " size - j");
-        for (int i = sectors.size() - j; i < sectors.size(); i++) {
-            proc.add(sectors.get(i));
-//            System.out.println(sectors.get(i) + " new sector");
-        }
-//        System.out.println(sum + " summ");
-        System.out.println(proc + " Основной");
-        proc.clear();
-        return sector;
-    }
 
-    static class Drawable extends JFrame {
-    Diagram_Window Dw = new Diagram_Window(getWidth(),getHeight());
-
+    /* private ArrayList<Double> sectors = new ArrayList<>();
+     private ArrayList<Double> proc = new ArrayList<>();
+     @Override
+     public double count(ArrayList<Double> list, double sum, double sector) {
+         double tm ;
+         int j = 0 ;
+         for (Double eDouble: list){
+               sum+=eDouble;
+           }
+         for (Double sDouble : list){
+             tm = sDouble/sum;
+             sectors.add(tm);
+             j++;
+         }
+ //        System.out.println(sectors.size() + " size sector");
+ //        System.out.println(j + " j");
+ //        System.out.println(sectors + " sector");
+ //        System.out.println(sectors.size() - j + " size - j");
+         for (int i = sectors.size() - j; i < sectors.size(); i++) {
+             proc.add(sectors.get(i));
+ //            System.out.println(sectors.get(i) + " new sector");
+         }
+ //        System.out.println(sum + " summ");
+         System.out.println(proc + " Основной");
+         proc.clear();
+         return sector;
+     }
+ */
+    static class Drawable extends JFrame implements ToCalculate {
         public void paint(@NotNull Graphics g) {
             setForeground(Color.green);
             g.fillArc(100, 100, 300, 300, 0, 120);
--- src/logic/ToCalculate.java
+++ src/logic/ToCalculate.java
@@ -3,5 +3,30 @@ package logic;
 import java.util.ArrayList;
 
 public interface ToCalculate {
-    double count(ArrayList<Double> list, double sum, double sector);
+    ArrayList<Double> sectors = new ArrayList<>();
+    ArrayList<Double> proc = new ArrayList<>();
+    default double count(ArrayList<Double> list, double sum, double sector) {
+        double tm;
+        int j = 0;
+        for (Double eDouble : list) {
+            sum += eDouble;
+        }
+        for (Double sDouble : list) {
+            tm = sDouble / sum;
+            sectors.add(tm);
+            j++;
+        }
+//        System.out.println(sectors.size() + " size sector");
+//        System.out.println(j + " j");
+//        System.out.println(sectors + " sector");
+//        System.out.println(sectors.size() - j + " size - j");
+        for (int i = sectors.size() - j; i < sectors.size(); i++) {
+            proc.add(sectors.get(i));
+//            System.out.println(sectors.get(i) + " new sector");
+        }
+//        System.out.println(sum + " summ");
+        System.out.println(proc + " Основной");
+        proc.clear();
+        return sector;
+    }
 }
