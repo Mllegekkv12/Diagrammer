f4f2816cd76b73898395f93b6f0a019bbf3a3062
Mllege <kiry.kkv12@gmail.com>
2019-11-21 06:32:35 +0700

    Diagram_Window -> MEIN WUNDER (Логика прорисовки)

--- src/Graphics/Diagram_Window.java
+++ src/Graphics/Diagram_Window.java
@@ -3,6 +3,7 @@ package Graphics;
 import abstraction.WindowFrame;
 import logic.ToCalculate;
 import org.jetbrains.annotations.NotNull;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -11,13 +12,23 @@ class Diagram_Window extends WindowFrame implements ToCalculate {
     Diagram_Window(int width, int height) {
         super(width, height);
     }
+
     static class Drawable extends JFrame implements ToCalculate {
         public void paint(@NotNull Graphics g) {
             int ty = 0;
+            g.drawArc(100, 100, 300, 300, ty, 360);
+            g.setColor(Color.blue);
+            if (Grad.size()<2){
+                g.drawArc(100, 100, 300, 300, ty, 360);
+            } else
             for (Integer integer : Grad) {
-                g.setColor(Color.blue);
-                g.fillArc(100, 100, 300, 300, ty, integer - 1);
-                ty += integer;
+            double fi = ty;
+            double radians = Math.toRadians(fi);
+            int x = (int) (250 + (150 * Math.cos(radians)));
+            int y = (int) (250 + (150 * Math.sin(radians)));
+            System.out.println(x+" x" + y +" y");
+            ty += integer;
+                g.drawLine(250,250,x ,y);
             }
         }
     }
@@ -25,10 +36,10 @@ class Diagram_Window extends WindowFrame implements ToCalculate {
         void Drawing() {
             javax.swing.SwingUtilities.invokeLater(() -> {
                 Drawable Dia = new Drawable();
-                Dia.setSize(800, 800);
+                Dia.setSize(500, 500);
                 Dia.setVisible(true);
                 Dia.setLocationRelativeTo(null);
-                Dia.setResizable(false);
+                Dia.setResizable(true);
             });
         }
     }
